2250990e6dc16a6370faae2de75309b27032340c
Roxane RAFFI KHANSARI <rraffi-k@paul-f3Ar3s7.clusters.42paris.fr>
2023-10-04 13:51:55 +0200

    env : ok

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -1,14 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rraffi-k <rraffi-k@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 17:48:42 by rraffi-k          #+#    #+#             */
-/*   Updated: 2023/10/04 10:29:00 by rraffi-k         ###   ########.fr       */
+/*   Updated: 2023/10/04 11:23:27 by rraffi-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "testshell.h"
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
@@ -19,6 +21,19 @@ int	ft_strcmp(const char *s1, const char *s2)
 		i++;
 	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
 }
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned int	i;
+
+	if (n == 0)
+		return (0);
+	i = 0;
+	while ((i < n - 1) && s1[i] && s2[i] && (s1[i] == s2[i]))
+		i++;
+	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+}
+
 
 /*
 int main()
--- testshell.h
+++ testshell.h
@@ -6,7 +6,7 @@
 /*   By: rraffi-k <rraffi-k@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 10:27:59 by rraffi-k          #+#    #+#             */
-/*   Updated: 2023/10/04 10:38:21 by rraffi-k         ###   ########.fr       */
+/*   Updated: 2023/10/04 11:51:55 by rraffi-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <aio.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 int	ft_strcmp(const char *s1, const char *s2);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strdup(const char *s);
 char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strnjoin(char const *s1, char const *s2, size_t n);
 size_t	ft_strlen(const char *s);
+char	**ft_split(char const *s, char c);
+char	**ft_free_double_char(char	**str, size_t	l);
+size_t	ft_cmpt(char const *s, char c);
 
 #endif
 
