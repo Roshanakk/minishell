53027de6a11d264c3efff821abddb8709e694fa7
Roxane RAFFI KHANSARI <rraffi-k@made-f0Br8s4.clusters.42paris.fr>
2023-10-13 11:48:10 +0200

    builtin echo : OK

--- src/builtin_echo.c
+++ src/builtin_echo.c
@@ -6,7 +6,7 @@
 /*   By: rraffi-k <rraffi-k@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:55:03 by rraffi-k          #+#    #+#             */
-/*   Updated: 2023/10/13 11:20:17 by rraffi-k         ###   ########.fr       */
+/*   Updated: 2023/10/13 11:47:29 by rraffi-k         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int respects_n_format(char *str)
 
 	if (!str)
 		return (0);
-	if (str[0] != '-')
+	if (str[0] != '-' || !str[1])
 		return (0);
 	i = 1;
 	while (str[i])
@@ -30,24 +30,12 @@ int respects_n_format(char *str)
 	return (1);
 }
 
-/*return la position du premier arg apres l'option -n (ou -nnnn)*/
-// int	check_n(char **args)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (args[i] && respects_n_format(args[i]))
-// 		i++;
-// 	return (i);
-// 	//if y a pas de -n : return (0)
-// 	// if y a un -n : return position du premier arg qui n'est pas -n
-// }
-
+/*   return la position du premier arg apres l'option -n (ou -nnnn, etc)    */
 int	check_n(char **args)
 {
 	int	i;
 
-	i = 2;
+	i = 1;
 	while (args[i])
 	{
 		if (!respects_n_format(args[i]))
@@ -55,42 +43,41 @@ int	check_n(char **args)
 		i++;
 	}
 	return (0);
-	//if y a pas de -n : return (0)
-	// if y a un -n : return position du premier arg qui n'est pas -n
 }
 
-// int	run_echo(char **args)
-// {
-// 	int	i;
-// 	int	n_option;
+/* 
+	ATTENTION
+   args = { "echo", "-n", "bonjour"}
+	donc args[0] = "echo" 
+*/
 
-// 	if (!args[1])
-// 	{
-// 		ft_putchar_fd('\n', 1);
-// 		return (1);
-// 	}
-// 	i = check_n(args);
-// }
-
-int main(int argc, char **argv)
+int	run_echo(char **args)
 {
-	// char test[] = "-nnnnn";
-	// char test[3][10] = {"echo", "-n", "bonjour"};
-	// char test1[] = "echo -nnnnn bonjour";
-	// char test2[] = "echo -n -n -n -n bonjour";
+	int	i;
+	int	n_option;
+	char c;
 
-	// char test3[] = "echo -n n n n bonjour";
-	// char test4[] = "echo -n-n-n bonjour";
-	// char test5[] = "echo -nno bonjour";
-	// char test6[] = "echo --nn bonjour";
-	// printf("1 ?\n");
-	printf("%d\n", check_n(argv));
-	// printf("%d\n", respects_n_format(argv));
-	// printf("%d\n", check_n(test1));
-	// printf("%d\n", check_n(test2));
-	// printf("0 ?\n");
-	// printf("%d\n", check_n(test3));
-	// printf("%d\n", check_n(test4));
-	// printf("%d\n", check_n(test5));
-	// printf("%d\n", check_n(test6));
+	if (!args[1])
+	{
+		ft_putchar_fd('\n', 1);
+		return (1);
+	}
+	i = check_n(args);
+	n_option = i > 1 ? 1 : 0;
+	while (args[i])
+	{
+		ft_putstr_fd(args[i], 1);
+		i++;
+		if (args[i])
+			ft_putchar_fd(' ', 1);
+	}
+	if (!n_option)
+		ft_putchar_fd('\n', 1);
+	return (n_option);
 }
+
+// int main(int argc, char **argv)
+// {
+// 	run_echo(argv);
+// 	// printf("%d\n", respects_n_format("-"));
+// }
